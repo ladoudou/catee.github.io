846ebd49162be7d644c254b0d1d6b05f628b0c21
catee <452416115@qq.com>
2014-09-30 16:32:26 +0800

    candycard
    RT

---
+++ catee/4u/js/main.js
@@ -0,0 +1,190 @@
+  // defs candyCard - start
+
+;(function (win, doc, $, _) {
+  function candyCard (conf) {
+    this.current = conf.startCard || 0;
+    //this.colorList = conf.colorList || ['rgba(255, 239, 213, 0.8)', 'rgba(240, 248, 255, 0.8)', 'rgba(255, 160, 122, 0.8)', 'rgba(107, 142, 35, 0.8)'];
+    this.cardListEle = $('.' + (conf.calssName || 'catee-candyCard'));
+    this.prevId = conf.prevId || 'catee_c_prev';
+    this.nextId = conf.nextId || 'catee_c_next';
+    this.cardNum = this.cardListEle.length;
+    this.eventArray = conf.eventArray || [];
+    this.timer = {};
+  }
+  candyCard.prototype = {
+    init: function () {
+      var self = this;
+      self.setAttributes();
+      //setTimeout(function () {
+      self.setPosition();
+        //self.setColors();
+      //}, 200);
+      self.bind(doc, 'mousewheel', self.scroll2Turn(self));
+      self.bind(doc, 'click', self.click2Turn(self));
+    },
+
+    /*setColors: function () {
+      var self = this,
+        cardListEle = self.cardListEle;
+      _.map(cardListEle, function (ele) {
+        $(ele).addClass('catee-animate-color');
+      })
+    },*/
+
+    setAttributes: function () {
+      var self = this,
+        cardListEle = self.cardListEle;
+      _.map(cardListEle, function (ele, i) {
+        ele.setAttribute('candyCard-index', i);
+        $(ele).addClass('catee-animate-init');
+      })
+    },
+
+    setPosition: function () {
+      var self = this,
+        length = self.cardNum,
+        current = self.current,
+        cardListEle = self.cardListEle;
+      _.map(cardListEle, function (ele, i) {
+        i && $(ele).attr('style', '-webkit-transition-delay:' + ((length - i) * 0.1) + 's').addClass('catee-init');
+        i && (i < 6) && $(ele).removeClass('catee-init').addClass(['catee-showout-', i].join(''));
+      })
+      setTimeout(function () {
+        cardListEle.attr('style', '-webkit-transition-delay:0.1s');
+      }, 100)
+    },
+
+    refreshPosition: function (type) {
+      var self = this,
+        current = 0,
+        length = self.cardNum,
+        cardListEle = self.cardListEle;
+      switch (type) {
+        case 1:
+          current = ++ self.current;
+          $(cardListEle[current]).addClass('catee-animate-active').removeClass('catee-showout-1 catee-init');
+          for (var i = 1, len = win.Math.min(6, length - 1 - current); i < len; i ++) {
+            $(cardListEle[current + i]).attr('style', '-webkit-transition-delay:' + (i * 0.1) + 's').addClass(['catee-showout-', i].join('')).removeClass([['catee-showout-', i + 1].join(''), ' catee-init'].join(''));
+          };
+          setTimeout(function () {
+            cardListEle.attr('style', '-webkit-transition-delay:0.1s');
+          }, 100);
+          break;
+        case 0:
+          current = self.current;
+          $(cardListEle[current]).addClass('catee-showout-0').removeClass('catee-animate-active');
+          setTimeout(function () {
+            for (var i = 0, len = win.Math.min(6, length - 1 - current); i < len; i ++) {
+              $(cardListEle[current + i]).attr('style', '-webkit-transition-delay:' + ((6 - i) * 0.1) + 's').addClass([['catee-showout-', i + 1].join(''), ' catee-init'].join('')).removeClass(['catee-showout-', i].join(''));
+            };
+            setTimeout(function () {
+              cardListEle.attr('style', '-webkit-transition-delay:0.1s');
+            }, 100);
+          }, 900);
+          self.current --;
+          break;
+        default:
+      }
+    },
+
+    setLocalStorage: function () {
+    },
+
+    getLocalStorage: function () {
+    },
+
+    checkUser: function () {
+    },
+
+    /*refreshColors: function () {
+      var self = this,
+        length = self.cardNum,
+        cardListEle = self.cardListEle,
+        colorList = self.colorList,
+        colorListLength = colorList.length,
+        colorIndex = self.getRandom(0, colorListLength, length);
+      _.map(cardListEle, function (ele, i) {
+        $(ele).css('background-color', colorList[colorIndex[i]]);
+      })
+    },*/
+
+    toNext: function () {
+      var self = this;
+      self.refreshPosition(1);
+    },
+
+    toPrev: function () {
+      var self = this;
+      self.refreshPosition(0);
+    },
+
+    openDrawer: function () {
+    },
+
+    closeDrawer: function () {
+    },
+
+    bind: function (ele, type, handle) {
+      var ele = typeof ele === 'string' ? $('#' + ele) : ele;
+      $(ele).on(type, handle);
+    },
+
+    unbind: function (ele, type, handle) {
+      var ele = typeof ele === 'string' ? $('#' + ele) : ele;
+      $(ele).off(type, handle);
+    },
+
+    scroll2Turn: function (that) {
+      return function (e) {
+        clearTimeout(that.timer);
+        that.timer = setTimeout(function () {
+          e.originalEvent.wheelDelta < 0 && that.toNext();
+          e.originalEvent.wheelDelta > 0 && that.toPrev();
+        }, 200)
+      }
+    },
+
+    click2Turn: function(that) {
+      return function (e) {
+        var t = e.target,
+          tId = t.id;
+        switch (tId) {
+          case that.nextId:
+            that.toNext();
+            break;
+          case that.prevId:
+            that.toPrev();
+            break;
+          default:
+        }
+      }
+    },
+
+    getRandom: function () {
+      var origin = (arguments[0] >> 0) || 0,
+        range = (arguments[1] >> 0) || 1,
+        length = win.Math.abs(arguments[2] >> 0) || 1,
+        result = [],
+        rdm = 0;
+        do {
+          rdm = origin + win.Math.floor((range - 1e-6) * win.Math.random());
+          result.push(rdm);
+        } while (-- length > 0)
+        return result;
+    }
+
+
+  }
+  win.candyCard = candyCard;
+})(window, document, $, _)
+
+  // defs candyCard - end
+
+  // defs Prairie - start
+  // defs Prairie - end
+
+
+
+var a = new candyCard({});
+a.init();
+//a.init();
---
+++ js/common/utils.js
@@ -0,0 +1,173 @@
+(function (win, doc) {
+  var utils = {
+    $: function(id){
+      return doc.getElementById(id);
+    },
+    fixEvent: function (e) {
+      var e = e || win.event;
+      e.target = e.target ? e.target : e.srcElement;
+      return e;
+    },
+    preventDefault: function (e) {
+      if (e.preventDefault) {
+        e.preventDefault();
+      } else {
+        e.returnValue = null;
+      }
+    },
+    hasClass: function(elem, cls){
+      var reg = new RegExp('(^|\\s)' + cls + '($|\\s)');
+      return reg.test(elem.className);
+    },
+    removeClass: function(elem, cls){
+      var reg = new RegExp('(^|\\s)' + cls + '($|\\s)', 'g');
+      elem.className = elem.className.replace(reg, ' ');
+    },
+    addClass: function(elem, cls){
+      if(!this.hasClass(elem, cls)){
+        elem.className += ' ' + cls;
+      }
+    },
+    toggleClass: function(ele, cls) {
+        if (this.hasClass(ele, cls)) {
+          this.removeClass(ele, cls);
+        } else {
+          this.addClass(ele, cls);
+        }
+      },
+    addEvent: doc.addEventListener ?
+      function (elem, type, fn) {
+        elem.addEventListener(type, fn, false);
+      } : function (elem, type, fn) {
+        elem.attachEvent('on' + type, fn);
+      },
+    removeEvent: doc.removeEventListener ?
+      function (elem, type, fn) {
+        elem.removeEventListener(type, fn, false);
+      } : function (elem, type, fn) {
+        elem.detachEvent('on' + type, fn);
+      },
+    getRelatedTarget: function (e) {
+          if (e.relatedTarget) {
+              return e.relatedTarget;
+          } else if (e.toElement) {
+              return e.toElement;
+          } else if (e.fromElement) {
+              return e.fromElement;
+          } else {
+              return null;
+          }
+      },
+    delegateByTag: function(ele,tag,etype,fn){
+      var that = this;
+      that.addEvent(ele,etype,function(ev){
+        var e = that.fixEvent(ev);
+        var t = e.target;
+        tag = tag.toLowerCase();
+        do {
+          if(t.nodeName && t.nodeName.toLowerCase() === tag){
+            fn(e, t);
+          }
+          t = t.parentNode;
+        } while (t && t !== ele)
+      });
+    },
+    bind: function(fn, context){
+      return function(){
+        fn.apply(context, arguments);
+      };
+    },
+    extend: function extend(obj1, obj2){
+      for(var key in obj2){
+        obj1[key] = obj2[key];
+      }
+    },
+    viewData: function(){
+      var e = 0, l = 0, i = 0, g = 0, f = 0, m = 0;
+      var j = win, h = doc, k = h.documentElement;
+      e = k.clientWidth || h.body.clientWidth || 0;
+      l = j.innerHeight || k.clientHeight || h.body.clientHeight || 0;
+      g = h.body.scrollTop || k.scrollTop || j.pageYOffset || 0;
+      i = h.body.scrollLeft || k.scrollLeft || j.pageXOffset || 0;
+      f = Math.max(h.body.scrollWidth, k.scrollWidth || 0);
+      m = Math.max(h.body.scrollHeight, k.scrollHeight || 0, l);
+      return {scrollTop: g,scrollLeft: i,documentWidth: f,documentHeight: m,viewWidth: e,viewHeight: l};
+    },
+    parseJSON: function(str){
+      if(typeof(str) === 'object') {
+        return str;
+      } else {
+        if(win.JSON){
+          return JSON.parse(str);
+        } else {
+          return eval('(' + str + ')');
+        }
+      }
+    },
+    twoFixNumber: function(n){
+      return n > 9 ? n : ('0' + n);
+    },
+    dateFormat: function(d){
+      var dt = new Date(win.parseInt(d, 10) * 1000);
+      return (dt.getMonth() + 1) + '\u6708' + dt.getDate() + '\u65E5 ' + utils.twoFixNumber(dt.getHours()) + ':' + utils.twoFixNumber(dt.getMinutes());
+    },
+    appendHTML: function(ele, html){
+      var divTemp = doc.createElement("div"), nodes = null, fragment = doc.createDocumentFragment();
+      divTemp.innerHTML = html;
+      nodes = divTemp.childNodes;
+      for (var i=0, length=nodes.length; i<length; i+=1) {
+         fragment.appendChild(nodes[i].cloneNode(true));
+      }
+      ele.appendChild(fragment);
+      nodes = null;
+      fragment = null;
+    },
+    getByClassName: function(str, root, tag) {
+      if (root) {
+        root = typeof root == "string" ? doc.getElementById(root) : root
+      } else {
+        root = doc.body
+      }
+      tag = tag || "*";
+      var els = root.getElementsByTagName(tag), arr = [];
+      for (var i = 0, n = els.length; i < n; i++) {
+        for (var j = 0, k = els[i].className.split(" "), l = k.length; j < l; j++) {
+           if (k[j] == str) {
+           arr.push(els[i]);
+           break
+        }}
+      }
+      return arr;
+    },
+
+    getByAttributeName: function(attrName, attrValue, root, tag){
+      if (root) {
+        root = typeof root == "string" ? doc.getElementById(root) : root
+      } else {
+        root = doc.body
+      }
+      tag = tag || "*";
+      var els = root.getElementsByTagName(tag), arr = [];
+      for (var i = 0, n = els.length; i < n; i++) {
+        if(els[i].getAttribute(attrName) == attrValue){
+          arr.push(els[i]);
+        }
+      }
+      return arr;
+    },
+    getPosition: function(ele){
+      var positionX=0;
+      var positionY=0;
+      while(ele!=null){
+        positionX+=ele.offsetLeft;
+        positionY+=ele.offsetTop;
+        ele=ele.offsetParent;
+      }
+      return {
+        x : positionX,
+        y : positionY
+      };
+    }
+  }
+  win.utils = utils;
+})(window, document);
